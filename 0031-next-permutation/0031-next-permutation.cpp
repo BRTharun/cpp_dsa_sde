7684c6daf4223936d0d39e0fdea7285273cacc4c
BRTharun <79271916+BRTharun@users.noreply.github.com>
2023-05-22 19:41:56 +0530

    Time: 8 ms (25.48%), Space: 12 MB (69.58%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int index=-1;
         int n=nums.size();
-        cout<<n<<endl;
+        int index=-1;
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 index=i;
                 break;
             }
         }
-        cout<<index;
         if(index==-1){
             reverse(nums.begin(),nums.end());
         }
         else{
-            for(int i=n-1;i>index;i--){
-                if(nums[index]<nums[i]){
-                swap(nums[i],nums[index]);
-                    break;
-            }       
+        for(int i=n-1;i>index;i--){
+            if(nums[index]<nums[i]){
+                swap(nums[index],nums[i]);
+                break;
+            }
         }
-            reverse(nums.begin() + index + 1,nums.end());
-        
+        reverse(nums.begin()+index+1,nums.end());
         }
-        
-        
     }
 };
