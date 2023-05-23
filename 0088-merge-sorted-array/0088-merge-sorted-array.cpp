e7df709b58da5fe87ae9154eafca58eb99b2780a
BRTharun <79271916+BRTharun@users.noreply.github.com>
2023-05-23 18:48:42 +0530

    Time: 0 ms (100.00%), Space: 9 MB (87.73%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
-    void swapIfGreater(vector<int> &arr1, vector<int> &arr2, int ind1, int ind2) {
-    if (arr1[ind1] > arr2[ind2]) {
-        swap(arr1[ind1], arr2[ind2]);
-    }
-}
+    
 
 void merge(vector<int> &arr1,int n, vector<int> &arr2,  int m) {
-    
-    
-    // len of the imaginary single array:
-    int len = n + m;
-
-    // Initial gap:
-    int gap = (len / 2) + (len % 2);
-
-    while (gap > 0) {
-        // Place 2 pointers:
-        int left = 0;
-        int right = left + gap;
-        while (right < len) {
-            // case 1: left in arr1[]
-            //and right in arr2[]:
-            if (left < n && right >= n) {
-                swapIfGreater(arr1, arr2, left, right - n);
-            }
-            // case 2: both pointers in arr2[]:
-            else if (left >= n) {
-                swapIfGreater(arr2, arr2, left - n, right - n);
-            }
-            // case 3: both pointers in arr1[]:
-            else {
-                swapIfGreater(arr1, arr1, left, right);
-            }
-            left++, right++;
+    int left=n-1;
+    int right=0;
+    while(left>=0 && right<m){
+        if(arr1[left]>arr2[right]){
+            swap(arr1[left],arr2[right]);
         }
-        // break if iteration gap=1 is completed:
-        if (gap == 1) break;
-
-        // Otherwise, calculate new gap:
-        gap = (gap / 2) + (gap % 2);
-        
+        else{
+            break;
+        }
+        left--,right++;
     }
-        int an=0;
-        for(int i=n;i<m+n;i++){
-            arr1[i]=arr2[an];
-            an++;
-        }
+    sort(arr2.begin(),arr2.end());
+    int in=0;
+    for(int i=n;i<n+m;i++){
+        arr1[i]=arr2[in];
+        in++;
+    }
+    sort(arr1.begin(),arr1.end());
+    
 }
 };
