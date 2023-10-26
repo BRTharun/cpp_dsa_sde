d283b89a4705dbc73c79c5fe211b764087be209b
BRTharun <79271916+BRTharun@users.noreply.github.com>
2023-10-26 19:31:22 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (11.56%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,45 +1,41 @@
-class Solution
-{
-    public:
-
-        void swapG(vector<int> &nums1, vector<int> &nums2, int a, int b)
-        {
-            if (nums1[a] > nums2[b])
-                swap(nums1[a], nums2[b]);
+class Solution {
+    
+public:
+     void swapGrater(vector<int>& nums1, vector<int>& nums2, int idx1,int idx2) {
+        if(nums1[idx1]>nums2[idx2]){
+            swap(nums1[idx1],nums2[idx2]);
         }
-    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
-
-        int len = m + n;
-        int gap = (len) / 2 + (len % 2);
-
-        while (gap > 0)
-        {
-            int left = 0, right = left + gap;
-
-            while (right < len)
-            {
-                if (left < m && right >= m)
-                {
-                    swapG(nums1, nums2, left, right - m);
+    }
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len=m+n;
+        int gap=(len)/2 + (len%2);
+        while(gap>0){
+            int left=0;
+            int right=left+gap;
+            while(right<len){
+                if(left<m && right>=m){
+                    swapGrater(nums1,nums2,left,right-m);
+                    
                 }
-                else if (left >= m)
-                {
-                    swapG(nums2, nums2, left - m, right - m);
+                else if(left>=m){
+                    swapGrater(nums2,nums2,left-m,right-m);
+                    
                 }
-                else
-                    swapG(nums1, nums1, left, right);
-                left++;
-                right++;
+                else{
+                    swapGrater(nums1,nums1,left,right);
+                    
+                }
+                left++,right++;
             }
-            if (gap == 1)
+            if(gap==1){
                 break;
-
-            gap = (gap / 2) + (gap % 2);
+            }
+            gap=(gap/2)+(gap%2);
         }
-
         int r = 0;
-        for (int i = m; i < m + n; i++)
+        for (int i = m; i < m + n; i++){
             nums1[i] = nums2[r++];
+    }
+        
     }
 };
